2e6ce417ba53b7ea3a334c999c0ef9b3728559f1
Nicole <lounic16@phahs.org.za>
2018-09-25 07:29:46 +0200

    Changed the TToken record to a class so it can have methods to move itsself around. Still a work in progress

--- TPlayerUnit_u.pas
+++ TPlayerUnit_u.pas
@@ -11,16 +11,23 @@ type
 type
   TPlayer = class
   type
-    TToken = record
+    TToken = class
       position: TImage;
 
       isSelected: boolean;
-      isinHome : boolean;
-      isInYard : boolean;
-      isInBoard : boolean;
+      isinHome: boolean;
+      isInYard: boolean;
+      isInBoard: boolean;
+
+      procedure MoveForward(numberOfSpaces: integer);
     end;
 
   var
+    RedToken1, RedToken2, RedToken3, RedToken4: TToken;
+    YellowToken1, YellowToken2, YellowToken3, YellowToken4: TToken;
+    BlueToken1, BlueToken2, BLueToken3, BlueToken4: TToken;
+    GreenToken1, GreenToken2, GreenToken3, GreenToken4: TToken;
+
     playerType: string;
     ListOfTokenPositions: array [0 .. 3] of TToken;
     StartSpace: TImage;
@@ -73,6 +80,11 @@ begin
 
         tokenPath := GetCurrentDir +
           '\Media\Token images\Red player 1 token.png';
+
+        RedToken1 := TToken.Create;
+        RedToken2 := TToken.Create;
+        RedToken3 := TToken.Create;
+        RedToken4 := TToken.Create;
       end;
 
     Yellow:
@@ -95,6 +107,11 @@ begin
         ActivePanelColor := clYellow_Board;
         tokenPath := GetCurrentDir +
           '\Media\Token images\Yellow player 2 token.png';
+
+        YellowToken1 := TToken.Create;
+        YellowToken2 := TToken.Create;
+        YellowToken3 := TToken.Create;
+        YellowToken4 := TToken.Create;
       end;
 
     Blue:
@@ -117,6 +134,11 @@ begin
         ActivePanelColor := clBlue_Board;
         tokenPath := GetCurrentDir +
           '\Media\Token images\Blue player 3 token.png';
+
+        BlueToken1 := TToken.Create;
+        BlueToken2 := TToken.Create;
+        BLueToken3 := TToken.Create;
+        BlueToken4 := TToken.Create;
       end;
 
     Green:
@@ -139,6 +161,11 @@ begin
         ActivePanelColor := clGreen_Board;
         tokenPath := GetCurrentDir +
           '\Media\Token images\Green player 4 token.png';
+
+        GreenToken1 := TToken.Create;
+        GreenToken2 := TToken.Create;
+        GreenToken3 := TToken.Create;
+        GreenToken4 := TToken.Create;
       end;
   end;
 end;
